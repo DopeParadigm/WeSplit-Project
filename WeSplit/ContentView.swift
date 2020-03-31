ab301ef751c461256a2f8e01388719f31032e627
Eric Williams II <beng985@gmail.com>
2020-03-31 14:03:46 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ben-G From The LPC on 3/31/20.
+//  Copyright © 2020 Ben-G From The LPC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
